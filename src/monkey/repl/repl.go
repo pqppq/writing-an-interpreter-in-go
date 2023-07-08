edcd2fd3ba8067609260de2fd74a75bc20e99990
pqppq <yutatsu0119@gmail.com>
2023-07-09 01:31:24 +0900

    feat: update repl

--- src/monkey/repl/repl.go
+++ src/monkey/repl/repl.go
@@ -6,7 +6,7 @@ import (
 	"io"
 
 	"github.com/pqppq/writing-an-interpreter-in-go/monkey/lexer"
-	"github.com/pqppq/writing-an-interpreter-in-go/monkey/token"
+	"github.com/pqppq/writing-an-interpreter-in-go/monkey/parser"
 )
 
 const PROMPT = ">> "
@@ -23,8 +23,24 @@ func Start(in io.Reader, out io.Writer) {
 
 		line := scanner.Text()
 		l := lexer.New(line)
-		for tok := l.NextToken(); tok.Type != token.EOF; tok = l.NextToken() {
-			fmt.Printf("%+v\n", tok)
+		p := parser.New(l)
+		program := p.ParseProgram()
+
+		if len(p.Errors()) != 0 {
+			printParserErrors(out, p.Errors())
+			continue
 		}
+
+		io.WriteString(out, program.String())
+		io.WriteString(out, "\n")
+	}
+}
+
+func printParserErrors(out io.Writer, errors []string) {
+	io.WriteString(out, "🙈🙊🙉\n")
+	io.WriteString(out, "Woops! We ran into some monkey business here!\n")
+	io.WriteString(out, " parser errors:\n")
+	for _, msg := range errors {
+		io.WriteString(out, "\t"+msg+"\n")
 	}
 }
