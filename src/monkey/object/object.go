7e088ecf9fa60ca6c9ea8685af1f513a240dc141
pqppq <yutatsu0119@gmail.com>
2023-07-21 22:52:23 +0900

    feat: add string evaluator

--- src/monkey/object/object.go
+++ src/monkey/object/object.go
@@ -16,6 +16,7 @@ const (
 	RETURN_VALUE_OBJ = "RETURN_VALUE"
 	ERROR_OBJ        = "ERROR"
 	FUNCTION_OBJ     = "FUNCTION"
+	STRING_OBJ       = "STRING"
 )
 
 type Object interface {
@@ -43,6 +44,17 @@ func (b *Boolean) Type() ObjectType {
 }
 func (b *Boolean) Inspect() string {
 	return fmt.Sprintf("%t", b.Value)
+}
+
+type String struct {
+	Value string
+}
+
+func (s *String) Type() ObjectType {
+	return STRING_OBJ
+}
+func (s *String) Inspect() string {
+	return s.Value
 }
 
 type Null struct{}
